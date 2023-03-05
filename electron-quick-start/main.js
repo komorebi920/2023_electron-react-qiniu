a6e08b6cfd607ea042c140b06b4cddd28ec7cfb7
komorebi920 <85144958+komorebi920@users.noreply.github.com>
2023-03-05 18:59:58 +0800

    electron 基本运用与 ipc 通信

--- electron-quick-start/main.js
+++ electron-quick-start/main.js
@@ -1,43 +1,70 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
-const path = require('path')
+const { app, BrowserWindow, ipcMain } = require("electron");
+const path = require("path");
 
-function createWindow () {
+function createWindow() {
+  // 加载 devtron
+  require("devtron").install();
+
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1600,
+    height: 1200,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
+      nodeIntegration: true,
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadFile("index.html");
 
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools();
+
+  // 监听 IPC 通信
+  ipcMain.on("message", (event, arg) => {
+    event.reply("reply", "主进程 reply message");
+
+    openSecondWindow(mainWindow);
+  });
+}
+
+/**
+ * 打开 second window
+ */
+function openSecondWindow(parent) {
+  let secondWindow = new BrowserWindow({
+    width: 400,
+    height: 300,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+    parent,
+  });
+
+  secondWindow.loadFile("second.html");
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  createWindow()
+  createWindow();
 
-  app.on('activate', function () {
+  app.on("activate", function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
-})
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") app.quit();
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
--- electron-quick-start/renderer.js
+++ electron-quick-start/renderer.js
@@ -5,3 +5,21 @@
  * `contextIsolation` is turned on. Use the contextBridge API in `preload.js`
  * to expose Node.js functionality from the main process.
  */
+const { ipcRenderer, remote } = require("electron");
+const { BrowserWindow } = remote;
+
+window.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("send").addEventListener("click", () => {
+    ipcRenderer.send("message", "渲染进程 send message");
+  });
+
+  document.getElementById("baidu").addEventListener("click", () => {
+    let win = new BrowserWindow({ width: 800, height: 600 });
+    win.loadURL("https://www.baidu.com");
+  });
+
+  // 监听 IPC 通信
+  ipcRenderer.on("reply", (event, arg) => {
+    document.getElementById("message").innerHTML = arg;
+  });
+});
